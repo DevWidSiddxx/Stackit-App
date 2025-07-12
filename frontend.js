3167b57a5aba828bbf95c4e1bbf3e85a57fa20b1
Siddharth Venkatesh <126441692+DevWidSiddxx@users.noreply.github.com>
2025-07-12 16:51:28 +0530

    frontend.js

---
+++ frontend.js
@@ -0,0 +1,8 @@
+// UI interaction functions, e.g.:
+function showQuestions() { /* ... */ }
+function showAskForm() { /* ... */ }
+function hideAskForm() { /* ... */ }
+function showQuestionDetail(questionId) { /* ... */ }
+function loadAnswers(questionId) { /* ... */ }
+function createAnswerCard(answer) { /* ... */ }
+// ...and all other UI logic from the <script> tag
